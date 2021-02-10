1c3d4c398b8a2b69b4079e09d80f8e55fcc1f111
Letícia Barbosa <leticiabarbosa94@gmail.com>
2021-02-10 00:45:11 -0300

    Rectangle class and its methods added

---
+++ PROG8170Assignment1/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PROG8170Assignment1
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+
+        public Rectangle()
+        {
+            this.length = 1;
+            this.width = 1;
+        }
+
+        public Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public int GetLength()
+        {
+            return length;
+        }
+
+        public int SetLength(int length)
+        {
+            this.length = length;
+            return this.length;
+        }
+
+        public int GetWidth()
+        {
+            return width;
+        }
+
+        public int SetWidth(int width)
+        {
+            this.width = width;
+            return this.width;
+        }
+
+        public int GetPerimeter()
+        {
+            return (width * 2) + (length * 2);
+        }
+
+        public int GetArea()
+        {
+            return width * length;
+        }
+    }
+}
